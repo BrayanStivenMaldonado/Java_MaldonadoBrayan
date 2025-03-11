dbe74861fffb0da17a79cbbca452a0a595d30134
Stiven Maldonado <165968813+BrayanStivenMaldonado@users.noreply.github.com>
2025-03-10 22:38:55 -0500

    :mag: Patrones de Diseño Estructurales

---
+++ ExplicacionDia14/src/PatronesDeDiseño_Estructurales/main.java
@@ -0,0 +1,17 @@
+package PatronesDeDiseño_Estructurales;
+
+public class main {
+    //Patrones de Diseño - Estructurales
+    //Se ocupan de cómo se compone y organizan las clases y objetos y para formar
+    //estructuras más grandes y complejas, pues ayudan a definir cómo las clases interactúan
+    //y se relacionan entre sí.
+    
+    //Estos son los siguientes patrones estructurales a revisar:
+    //1. Adapter
+    //2. Bridge
+    //3. Composite
+    //4. Decorator
+    //5. Facade
+    //6. Flyweight
+    //7. Proxy
+}
