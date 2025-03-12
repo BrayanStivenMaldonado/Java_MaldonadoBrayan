79afb466552dce0af75038de6517e221f1c2644d
Stiven Maldonado <165968813+BrayanStivenMaldonado@users.noreply.github.com>
2025-03-11 21:26:58 -0500

    :mag: Patrones de Diseño Creacionales

---
+++ ExplicacionDia15/src/PatronesDiseno_Comportamentales/PatronesComportamentales.java
@@ -0,0 +1,14 @@
+package PatronesDiseno_Comportamentales;
+
+public class PatronesComportamentales {
+    
+    //Patrones de Comportamiento
+    //Son patrones que se enfocan en cómo las clases y objetos interactúan y se comunican entre sí
+    //para lograr diferentes comportamientos y funcionalidades. Estas proporcionarán soluciones para gestionar 
+    //la colaboración y coordinación entre las clases.
+    
+    //Formalizaremos los siguientes patrones:
+    //1. Chain of Command (Patrón de Comando)
+    //2. Iterator
+    //3. Mediator
+}
